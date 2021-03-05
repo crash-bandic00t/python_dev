f5aecc8f6d02dc3afb9796af6372ff6e7d33c8ed
CrashB <sirotin92@yandex.ru>
2021-03-05 14:41:24 +0300

    homework lesson 3

--- 1 fourth/basics_python/lesson 3/task1.py
+++ 1 fourth/basics_python/lesson 3/task1.py
@@ -1,1 +1,23 @@
-pass
+# Написать функцию num_translate(), переводящую числительные от 0 до 10 c английского на русский язык. Например:
+# >>> >>> num_translate("one")
+# "один"
+# >>> num_translate("eight")
+# "восемь"
+
+def num_translate(number):
+    dictionary = {
+        'zero':'ноль',
+        'one':'один',
+        'two':'два',
+        'three':'три',
+        'four':'четыре',
+        'five':'пять',
+        'six':'шесть',
+        'seven':'семь',
+        'eight':'восемь',
+        'nine':'девять',
+        'ten':'десять',
+    }
+    print(dictionary.get(number))
+
+num_translate(input('Введите число на английском языке: \n'))
