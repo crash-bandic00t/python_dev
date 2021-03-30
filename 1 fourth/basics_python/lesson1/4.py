2800ff28e054df604fd0c22e25374126e76a4a42
CrashB <sirotin92@yandex.ru>
2021-03-30 14:46:29 +0300

    fix master

--- 1 fourth/basics_python/lesson1/4.py
+++
@@ -1,8 +0,0 @@
-for i in range(101):
-    num = str(i)
-    if num[-1] == '0'  or num[-1] == '5' or num[-1] == '6' or num[-1] == '7' or num[-1] == '8' or num[-1] == '9':
-        print(f'{i} процентов')
-    elif num[-1] == '2' or num[-1] == '3' or num[-1] == '4':
-        print(f'{i} процента')
-    elif num[-1] == '1':
-        print(f'{i} процент')
